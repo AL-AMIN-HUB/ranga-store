f2652413aeafbab9cc85bab12559a6a498b4dbdb
Md-Alamin-hub <86272640+Md-Alamin-hub@users.noreply.github.com>
2021-09-13 15:31:58 +0600

    design better

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 const loadProducts = () => {
-  const url = `https://fakestoreapi.com/products`;
+  const url = `https://fakestoreapi.com/products/`;
   fetch(url)
     .then((response) => response.json())
     .then((data) => showProducts(data));
@@ -18,7 +18,9 @@ const showProducts = (products) => {
     <img class="product-image" src=${image}></img>
       </div>
       <h3>${product.title}</h3>
-      <p>Category: ${product.category}</p>
+      <p>Category: <span class= "category">${product.category}</span></p>
+      <p>Rating: <i class="fas fa-star"></i> <i class="fas fa-star"></i> <i class="fas fa-star"></i> <i class="fas fa-star"></i> <i class="fas fa-star-half-alt"></i>${product.rating.rate}</p>
+      <p>Total Review: <i class="fas fa-user"></i> ${product.rating.count}</p>
       <h2>Price: $ ${product.price}</h2>
       <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
       <button id="details-btn" class="btn btn-danger">Details</button></div>
@@ -46,7 +48,7 @@ const updatePrice = (id, value) => {
   const convertedOldPrice = getInputValue(id);
   const convertPrice = parseFloat(value);
   const total = convertedOldPrice + convertPrice;
-  document.getElementById(id).innerText = Math.round(total);
+  document.getElementById(id).innerText = total;
 };
 
 // set innerText function
